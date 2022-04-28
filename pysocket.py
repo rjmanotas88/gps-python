02e207725945a4140d2348a6b78103198fca9887
Roberth Manotas <rmanotas@geotech.com.co>
2022-04-28 17:45:58 -0500

    cambios en server

--- pysocket.py
+++ pysocket.py
@@ -20,6 +20,3 @@ while True:
     print("####### Server is listening #######")
     data, address = s.recvfrom(4096)
     print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
-    send_data = input("Type some text to send => ")
-    s.sendto(send_data.encode('utf-8'), address)
-    print("\n\n 1. Server sent : ", send_data,"\n\n")
--- python.py
+++ python.py
@@ -9,4 +9,4 @@ trama = "TEVA"
 #s.send(binascii.unhexlify('830546340797010101010700000A00000000000000')) #HEX
 s.send(trama.encode("utf-8")) #ASCII
 
-#s.close()
+#s.close()ls
