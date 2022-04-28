5023ff72b4cf6075b6eff77928cba2f418b0ae7e
Roberth Manotas <rmanotas@geotech.com.co>
2022-04-28 16:04:22 -0500

    inital commit

---
+++ python.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import socket
+import binascii
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
+s.connect(('api.geotech.com.co', 6000))
+trama = "TEVA"
+#s.send(binascii.unhexlify('830546340797010101010700000A00000000000000')) #HEX
+s.send(trama.encode("utf-8")) #ASCII
+
+#s.close()
